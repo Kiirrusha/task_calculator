512b0594b25a059106628ce4f81fda184a421c8c
Kiirrusha <kiryll.rudenkov@yandex.by>
2023-09-06 14:35:34 +0300

    beta_commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const inputANode = document.querySelector("number_a");
-const inputBNode = document.querySelector("number_b");
-const inputButtonNode = document.querySelector("button");
-const inputOutputNode = document.querySelector("output");
+// const inputANode = document.querySelector("number_a");
+// const inputBNode = document.querySelector("number_b");
+// const inputButtonNode = document.querySelector("button");
+// const inputOutputNode = document.querySelector("output");
+const display = document.querySelector(".display_input");
+const buttons = array.from(document.querySelectorAll(row_button)); 
