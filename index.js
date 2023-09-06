2c78b42248959105e87c752d73a1604fd772fac6
Kiirrusha <kiryll.rudenkov@yandex.by>
2023-09-06 12:58:45 +0300

    beta_test

---
+++ index.js
@@ -0,0 +1,4 @@
+const inputANode = document.querySelector("number_a");
+const inputBNode = document.querySelector("number_b");
+const inputButtonNode = document.querySelector("button");
+const inputOutputNode = document.querySelector("output");
---
+++ math.js
@@ -0,0 +1,39 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function substract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+
+const operations = {
+    sum: "+",
+    substract: "-",
+    multiply: "*",
+    divide: "/",
+}
+
+function calculator (a, b, operations) {
+    let result = null;
+
+    if (operations == sum) {
+        result = sum(a, b);
+    } if else (operations == substract) {
+        result = substract(a, b);
+    } if else (operations == multiply) {
+        result = multiply(a, b);
+    } if else (operations == divide) {
+        result = divide(a, b);
+    }
+
+    return result;
+}
